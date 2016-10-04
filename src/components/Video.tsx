7a05902a36fcc2c26721b39f6c7cd05c78e63c7b
alexvictoor <alexvictoor@gmail.com>
2016-10-04 22:47:37 +0200

    Fix memory leak related to the html video player

---
+++ src/components/Video.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+
+interface VideoProps {
+    source: string;
+}
+
+// Check out this stackoverflow thread to understand why this component
+// http://stackoverflow.com/questions/3258587/how-to-properly-unload-destroy-a-video-element/28060352#28060352
+export default class Video extends React.Component<VideoProps, any> {
+  
+  private videoElt: HTMLVideoElement;
+
+  constructor(props : VideoProps) {
+    super(props)
+  }
+   
+  componentWillUnmount() : void {
+    this.videoElt.pause();
+    this.videoElt.src = "";
+    this.videoElt.load();
+  }
+  
+  render() {
+    const { source } = this.props; 
+    return (
+      <video key="build_highlight_video" autoPlay loop style={{maxHeight: "100%", maxWidth: "100%" }} 
+        ref={(ref) => this.videoElt = ref}
+      >
+        <source src={source} />
+      </video>
+    );
+  }
+}
