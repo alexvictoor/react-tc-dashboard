8d0882725f0baadcb7013faee98a1f9470223f8c
alexvictoor <alexvictoor@gmail.com>
2016-10-13 22:37:34 +0200

    clean up

--- src/clock.ts
+++
@@ -1,8 +0,0 @@
-import { Action, types, BuildNotification } from "./actions"
-
-export default (state: Date = new Date(), action: Action<any>) : Date => {
-  if (action && action.type === types.CLOCK_TICK) {
-    return  (action as Action<Date>).payload;
-  }
-  return state;
-}
